42fd7eaef0ec26e2b4d56e0ec8555cdccf28505c
Daniel O'Hanlon <dpohanlon@gmail.com>
2011-11-23 14:07:10 +0000

    Code is now nicer. Or worse. I don't know.

--- ris2bib.py
+++ ris2bib.py
@@ -7,8 +7,10 @@ ris2bib.py
 Takes .ris files as first argument in the form 'name.ris' and outputs files in the bibtex format in the form 'name.bib'.
 Currently assuming Nature article format for ease of programming.
 
+Usage is 'python ris2bib.py /path/to/references/file.ris (-v)'
+
 D.P O'Hanlon
-19/10/2011
+23/10/2011
 
 """
 
@@ -17,53 +19,67 @@ import os
 import re
 import string
 
-def r2b():
-
-	authors=[]
-
-	ris = open(sys.argv[1],'r+')
-
-	for line in ris:
-		if re.match("PY",line):
-			year = line[6:10]
-		elif re.match("AU",line):
-			authors.append(line[6:-1]) # goes from end of AU designation to end minus one to remove newline character
-		elif re.match("VL",line):
-			volume = line[6:-1]
-		elif re.match("TI",line):
-			title = line[6:-1]
-		elif re.match("JA",line):
-			journal = line[6:-1]
-		elif re.match("IS",line):
-			number = line[6:-1]
-		elif re.match("SP",line):
-			startpage = line[6:-1]
-		elif re.match("EP",line):
-			endpage = line[6:-1]
-		elif re.match("UR",line):
-			url = line[6:-1]
-		else:
-			print "Unparsed line: " + line
+def main(argv=sys.argv):
+	ris = open(argv[1],'r+')
+	
+	entries = r2b_read(ris, argv)
 
 	ris.close()
 
-	bib = open(sys.argv[1][:-4]+'.bib','w+') # strip and replace extension
+	bib_filename = argv[1][:-4]+'.bib' # strip and replace extension
 
-	bib.write('@ARTICLE{' + authors[0][:string.find(authors[0], ',')] + str(year) + ",") #takes surname of first author via slicing to ','
-	bib.write('\nauthor=\t\"'+authors[0])
-	for entry in authors[1:]:
+	r2b_write(entries, bib_filename)
+
+def r2b_read(ris,argv):
+	entries = dict()
+	entries['authors']=list()
+	verbose = False
+
+	if len(argv)==3:
+		if argv[2]=='-v':
+			verbose = True
+
+	for line in ris:
+		if re.match("PY",line):
+			entries['year'] = line[6:10]
+		elif re.match("AU",line):
+			entries['authors'].append(line[6:-1]) # goes from end of AU designation to end minus one to remove newline character
+		elif re.match("VL",line):
+			entries['volume'] = line[6:-1]
+		elif re.match("TI",line):
+			entries['title'] = line[6:-1]
+		elif re.match("JA",line):
+			entries['journal'] = line[6:-1]
+		elif re.match("IS",line):
+			entries['number'] = line[6:-1]
+		elif re.match("SP",line):
+			entries['startpage'] = line[6:-1]
+		elif re.match("EP",line):
+			entries['endpage'] = line[6:-1]
+		elif re.match("UR",line):
+			entries['url'] = line[6:-1]
+		elif verbose==True:
+			print 'Unparsed line: ' + line[:-1]
+	return entries
+		
+def r2b_write(entries,bib_filename):
+	bib = open(bib_filename,'w+') # strip and replace extension
+
+	bib.write('@ARTICLE{' + entries['authors'][0][:string.find(entries['authors'][0], ',')] + str(entries['year']) + ",") #takes surname of first author via slicing to ','
+	bib.write('\nauthor=\t\"'+entries['authors'][0])
+	for entry in entries['authors'][1:]:
 		bib.write(" and " + entry)
 	bib.write("\",")
-	bib.write('\nyear=\t\"'+ year + "\",")
-	bib.write("\ntitle=\t\"" + title + "\",")
-	bib.write("\njournal=\t\"" + journal + "\",")
-	bib.write("\nvolume=\t\"" + volume + "\",")
-	bib.write("\nnumber=\t\"" + number + "\",")
-	bib.write("\npages=\t\"" + startpage + "--" + endpage + "\",")
-	bib.write("\nurl=\t\"" + url + "\",")
+	bib.write('\nyear=\t\"'+ entries['year'] + "\",")
+	bib.write("\ntitle=\t\"" + entries['title'] + "\",")
+	bib.write("\njournal=\t\"" + entries['journal'] + "\",")
+	bib.write("\nvolume=\t\"" + entries['volume'] + "\",")
+	bib.write("\nnumber=\t\"" + entries['number'] + "\",")
+	bib.write("\npages=\t\"" + entries['startpage'] + "--" + entries['endpage'] + "\",")
+	bib.write("\nurl=\t\"" + entries['url'] + "\",")
 	bib.write("\n}\n")
 
 	bib.close()			
 
 if __name__ == '__main__':
-	r2b()
+	main()
