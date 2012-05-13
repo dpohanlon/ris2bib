858e4b15645872b6fe5030343c35e3b2070d5afb
Daniel O'Hanlon <dpohanlon@gmail.com>
2012-05-13 14:15:58 +0100

    Fixed bug cause by missing ':', added verbose mode and added GPL license

--- ris2bib.py
+++ ris2bib.py
@@ -4,13 +4,33 @@
 """
 ris2bib.py
 
-Takes .ris files as first argument in the form 'name.ris' and outputs files in the bibtex format in the form 'name.bib'.
-Currently assuming Nature article format for ease of programming.
+Copyright (C) 2011, 2012 Daniel O'Hanlon	
 
-Usage is python ris2bib.py [FILE...]
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+"""
+Takes .ris files as first argument in the form 'name.ris' and outputs files
+in the bibtex format in the form 'name.bib'. Currently assuming Nature article
+format for ease of programming.
+
+Uses .ris tag information from: http://en.wikipedia.org/wiki/RIS_(file_format)
+
+Usage is ris2bib.py [FILE] [-v]
 
 D.P O'Hanlon
-23/11/2011
+Sun 13 May 2012 14:10:52 BST
 
 """
 
@@ -19,27 +39,46 @@ import os
 import re
 import string
 
-def main(argv=sys.argv):
-	ris = open(argv[1],'r+')
-	
-	entries = r2b_read(ris, argv)
+def main(argv = sys.argv):
 
-	ris.close()
+	argc = len(argv)
 
-	bib_filename = argv[1][:-4]+'.bib' # strip and replace extension
+	if (argc == 1):
+		print "Usage is ris2bib.py [FILE] [-v]"
+	else:
+		try:
+			ris = open(argv[1],'r+')
+		except:
+			print "Error: No such file."
+			return
+		
+		if (argc == 3 and argv[2] == '-v'):
+			verbose = True
+		else:
+			verbose = False
 
-	r2b_write(entries, bib_filename)
+		entries = r2b_read(ris, verbose)
 
-def r2b_read(ris,argv):
+		ris.close()
+
+		bib_filename = argv[1][:-4]+'.bib' # strip and replace extension
+
+		r2b_write(entries, bib_filename)
+
+def r2b_read(ris, verbose):
+
+	"""
+	Reads in a .ris file and returns a .bib format 'entries' dictionary with the appropriate information.
+	"""
+
 	entries = dict()
-	entries['authors']=list()
-	verbose = False
+	entries['authors']=list() # Allows for multiple authors
 
 	for line in ris:
 		if re.match("PY",line):
 			entries['year'] = line[6:10]
 		elif re.match("AU",line):
-			entries['authors'].append(line[6:-1]) # goes from end of AU designation to end minus one to remove newline character
+			entries['authors'].append(line[6:-1]) # minus one to remove newline
 		elif re.match("VL",line):
 			entries['volume'] = line[6:-1]
 		elif re.match("TI",line):
@@ -54,14 +93,20 @@ def r2b_read(ris,argv):
 			entries['endpage'] = line[6:-1]
 		elif re.match("UR",line):
 			entries['url'] = line[6:-1]
-		else
+		elif verbose:
 			print 'Unparsed line: ' + line[:-1]
 	return entries
 		
 def r2b_write(entries,bib_filename):
+
+	"""
+	Writes the .bib formatted dictionary to file using .bib file syntax.
+	"""
+
 	bib = open(bib_filename,'w+') # strip and replace extension
 
-	bib.write('@ARTICLE{' + entries['authors'][0][:string.find(entries['authors'][0], ',')] + str(entries['year']) + ",") #takes surname of first author via slicing to ','
+	bib.write('@ARTICLE{' + entries['authors'][0][:string.find(entries['authors'][0], ',')] + \
+		str(entries['year']) + ",") # get surname of first author slicing to ','
 	bib.write('\nauthor=\t\"'+entries['authors'][0])
 	for entry in entries['authors'][1:]:
 		bib.write(" and " + entry)
@@ -71,7 +116,8 @@ def r2b_write(entries,bib_filename):
 	bib.write("\njournal=\t\"" + entries['journal'] + "\",")
 	bib.write("\nvolume=\t\"" + entries['volume'] + "\",")
 	bib.write("\nnumber=\t\"" + entries['number'] + "\",")
-	bib.write("\npages=\t\"" + entries['startpage'] + "--" + entries['endpage'] + "\",")
+	bib.write("\npages=\t\"" + entries['startpage'] + "--" + \
+		entries['endpage'] + "\",")
 	bib.write("\nurl=\t\"" + entries['url'] + "\",")
 	bib.write("\n}\n")
 
